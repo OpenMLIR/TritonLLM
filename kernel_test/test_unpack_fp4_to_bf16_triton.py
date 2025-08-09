3bbbb313fa7dcf9008280fec351b82287af95910
BobHuang <git@bobhuang.xyz>
2025-08-09 13:49:51 +0800

    update test

--- kernel_test/test_unpack_fp4_to_bf16_triton.py
+++ kernel_test/test_unpack_fp4_to_bf16_triton.py
@@ -50,7 +50,7 @@ def _unpack_fp4_to_bf16_triton_hopper_and_later(x):
 
 @triton.jit
 def _unpack_fp4_to_bf16_triton_before_hopper(x):
-    # A100 support is possible via fmul
+    # Ampere support with fmul
     r0, r1 = tl.inline_asm_elementwise(
         r"""
         {
@@ -60,18 +60,16 @@ def _unpack_fp4_to_bf16_triton_before_hopper(x):
             mov.b32 bias, 0x7e807e80; // 2 ** 126 == 2 ** (bias_bf16 - bias_fp2)
             // We add the missing bias to the scale directly
             and.b32 $0, $4, 0b10000001110000001000000111000000;
-            cvt.b16.b32 low, $0;
-            cvt.b16.b32 t16, bias;
+            cvt.u16.u32 low, $0;
+            cvt.u16.u32 t16, bias;
             cvt.f32.bf16 temp, low;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 low, temp;
-            // mul.bf16 low, low, t16;
             shr.u32 temp, $0, 16;
-            cvt.b16.b32 high, temp;
+            cvt.u16.u32 high, temp;
             shr.u32 temp, bias, 16;
-            cvt.b16.b32 t16, temp;
-            // mul.bf16 high, high, t16;
+            cvt.u16.u32 t16, temp;
             cvt.f32.bf16 temp, high;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
@@ -80,53 +78,46 @@ def _unpack_fp4_to_bf16_triton_before_hopper(x):
             cvt.u32.u16 temp, high;
             shl.b32 temp, temp, 16;
             or.b32 $0, $0, temp;
-            // mul.bf16x2 $0, $0, bias;
             shl.b32 b, $4, 3;
             and.b32 $1, b,  0b10000001110000001000000111000000;
-            cvt.b16.b32 low, $1;
-            cvt.b16.b32 t16, bias;
+            cvt.u16.u32 low, $1;
+            cvt.u16.u32 t16, bias;
             cvt.f32.bf16 temp, low;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 low, temp;
-            // mul.bf16 low, low, t16;
             shr.u32 temp, $1, 16;
-            cvt.b16.b32 high, temp;
+            cvt.u16.u32 high, temp;
             shr.u32 temp, bias, 16;
-            cvt.b16.b32 t16, temp;
+            cvt.u16.u32 t16, temp;
             cvt.f32.bf16 temp, high;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 high, temp;
-            // mul.bf16 high, high, t16;
             cvt.u32.u16 $1, low;
             cvt.u32.u16 temp, high;
             shl.b32 temp, temp, 16;
             or.b32 $1, $1, temp;
-            // mul.bf16x2 $1, $1, bias;
             shl.b32 c, $4, 6;
             and.b32 $2, c,  0b10000001110000001000000111000000;
-            cvt.b16.b32 low, $2;
-            cvt.b16.b32 t16, bias;
+            cvt.u16.u32 low, $2;
+            cvt.u16.u32 t16, bias;
             cvt.f32.bf16 temp, low;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 low, temp;
-            // mul.bf16 low, low, t16;
             shr.u32 temp, $2, 16;
-            cvt.b16.b32 high, temp;
+            cvt.u16.u32 high, temp;
             shr.u32 temp, bias, 16;
-            cvt.b16.b32 t16, temp;
+            cvt.u16.u32 t16, temp;
             cvt.f32.bf16 temp, high;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 high, temp;
-            // mul.bf16 high, high, t16;
             cvt.u32.u16 $2, low;
             cvt.u32.u16 temp, high;
             shl.b32 temp, temp, 16;
             or.b32 $2, $2, temp;
-            // mul.bf16x2 $2, $2, bias;
             // Unpack last two elements
             shl.b32 d0, $4, 1;
             and.b32 d1, d0, 0b10000000000000001000000000000000;
@@ -136,27 +127,24 @@ def _unpack_fp4_to_bf16_triton_before_hopper(x):
             shr.b32 d5, $4, 7;
             and.b32 d6, d5, 0b00000000010000000000000001000000;
             or.b32 $3, d4, d6;
-            cvt.b16.b32 low, $3;
-            cvt.b16.b32 t16, bias;
+            cvt.u16.u32 low, $3;
+            cvt.u16.u32 t16, bias;
             cvt.f32.bf16 temp, low;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 low, temp;
-            // mul.bf16 low, low, t16;
             shr.u32 temp, $3, 16;
-            cvt.b16.b32 high, temp;
+            cvt.u16.u32 high, temp;
             shr.u32 temp, bias, 16;
-            cvt.b16.b32 t16, temp;
+            cvt.u16.u32 t16, temp;
             cvt.f32.bf16 temp, high;
             cvt.f32.bf16 temp1, t16;
             mul.f32 temp, temp, temp1;
             cvt.rn.bf16.f32 high, temp;
-            // mul.bf16 high, high, t16;
             cvt.u32.u16 $3, low;
             cvt.u32.u16 temp, high;
             shl.b32 temp, temp, 16;
             or.b32 $3, $3, temp;
-            // mul.bf16x2 $3, $3, bias;
         }
         """,
         constraints="=r,=r,=r,=r,r",
@@ -196,5 +184,5 @@ if __name__ == "__main__":
     y1 = torch.empty((64, 256), dtype=torch.bfloat16, device="cuda")
     kernel_unpack1[1,](x, y1)
     y2 = torch.empty((64, 256), dtype=torch.bfloat16, device="cuda")
-    kernel_unpack1[1,](x, y2)
+    kernel_unpack2[1,](x, y2)
     assert_close(y1, y2)
