82504e992d55422cbf1c51b96d59bfcf2e5b0023
BobHuang <30999153+sBobHuang@users.noreply.github.com>
2025-08-15 06:05:35 +0800

    Add RMSNorm and ROPE kernel (#13)

--- gpt_oss/triton/model.py
+++ gpt_oss/triton/model.py
@@ -9,6 +9,7 @@ from torch.profiler import record_function
 from gpt_oss.triton.weights import Checkpoint
 from gpt_oss.triton.attention import attention, attention_ref
 from gpt_oss.triton.moe import quantize_mx4, moe
+from gpt_oss.triton.triton_kernels import rmsnorm_forward, rope_forward
 
 @dataclass
 class ModelConfig:
@@ -40,11 +41,9 @@ class RMSNorm(torch.nn.Module):
             torch.ones(num_features, device=device, dtype=torch.float32)
         )
 
+    @record_function("rmsnorm")
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        assert x.shape[-1] == self.num_features
-        t, dtype = x.float(), x.dtype
-        t = t * torch.rsqrt(torch.mean(t**2, dim=-1, keepdim=True) + self.eps)
-        return (t * self.scale).to(dtype)
+        return rmsnorm_forward(x, self.scale, self.eps)
 
 
 class RotaryEmbedding(torch.nn.Module):
@@ -141,16 +140,7 @@ class RotaryEmbedding(torch.nn.Module):
         key: torch.Tensor,
         offset: torch.LongTensor,
     ) -> tuple[torch.Tensor, torch.Tensor]:
-        batch_size, num_tokens, num_heads, head_dim = query.shape
-        batch_size, num_tokens, num_key_value_heads, head_dim = key.shape
-
-        idx = torch.arange(num_tokens, device=query.device, dtype=torch.long) + offset
-        idx = idx % self.max_context_length
-        cos = self.cos.index_select(0, idx)
-        sin = self.sin.index_select(0, idx)
-
-        query = self._rotate(query, cos, sin)
-        key = self._rotate(key, cos, sin)
+        rope_forward(query, key, self.sin, self.cos, self.max_context_length, offset)
         return query, key
 
 
---
+++ gpt_oss/triton/triton_kernels.py
@@ -0,0 +1,106 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def rmsnorm(x_ptr, t_ptr, scale_ptr, last_dim, eps, BLOCK_SIZE: tl.constexpr):
+    row = tl.program_id(0)
+    x_ptr = x_ptr + row * last_dim
+    t_ptr = t_ptr + row * last_dim
+    _sum = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
+    for off in range(0, last_dim, BLOCK_SIZE):
+        cols = off + tl.arange(0, BLOCK_SIZE)
+        x = tl.load(x_ptr + cols, mask=cols < last_dim, other=0)
+        _sum += x * x
+    mean = tl.sum(_sum, axis=0) / last_dim + eps
+    pid = tl.program_id(1)
+    offset = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    x = tl.load(x_ptr + offset, mask=offset < last_dim, other=0)
+    scale = tl.load(scale_ptr + offset, mask=offset < last_dim, other=0)
+    y = x * tl.math.rsqrt(mean) * scale
+    tl.store(t_ptr + offset, y, mask=offset < last_dim)
+
+
+def rmsnorm_forward(x, scale, eps):
+    t = torch.empty_like(x)
+    last_dim = x.shape[-1]
+    remaining = 1
+    for s in x.shape[:-1]:
+        remaining *= s
+    grid = lambda META: (remaining, triton.cdiv(last_dim, META['BLOCK_SIZE']))
+    rmsnorm[grid](x, t, scale, last_dim, eps, BLOCK_SIZE=128)
+    return t
+
+
+@triton.jit
+def rotate(
+    query_ptr,
+    key_ptr,
+    sin_ptr,
+    cos_ptr,
+    offset_ptr,
+    max_context_length,
+    num_tokens,
+    num_heads: tl.constexpr,
+    head_dim: tl.constexpr,
+    num_key_value_heads: tl.constexpr,
+    head_dim_div: tl.constexpr,
+    TILE_TOKENS: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    pid_t = tl.program_id(1)
+    query_ptr += pid * num_tokens * num_heads * head_dim
+    key_ptr += pid * num_tokens * num_key_value_heads * head_dim
+    offs_token = pid_t * TILE_TOKENS + tl.arange(0, TILE_TOKENS)[:, None]
+    offset = tl.load(offset_ptr)
+    new_offs_token = (offs_token + offset) % max_context_length
+    offs_dim_div = tl.arange(0, head_dim_div)[None, :]
+    offs = new_offs_token * head_dim_div + offs_dim_div
+    sin_cos_mask = offs_token < num_tokens
+    sin = tl.load(sin_ptr + offs, mask=sin_cos_mask)
+    sin = sin[:, None, :]
+    cos = tl.load(cos_ptr + offs, mask=sin_cos_mask)
+    cos = cos[:, None, :]
+
+    offs_token = pid_t * TILE_TOKENS + tl.arange(0, TILE_TOKENS)[:, None, None]
+    offs_head = tl.arange(0, num_heads)[None, :, None]
+    offs_dim = tl.arange(0, head_dim_div)[None, None, :]
+    offs = offs_token * (num_heads * head_dim) + offs_head * head_dim + offs_dim
+    q_k_mask = offs_token < num_tokens
+    q1 = tl.load(query_ptr + offs, mask=q_k_mask)
+    q2 = tl.load(query_ptr + offs + head_dim_div, mask=q_k_mask)
+    o1 = q1 * cos - q2 * sin
+    o2 = q2 * cos + q1 * sin
+    tl.store(query_ptr + offs, o1, mask=q_k_mask)
+    tl.store(query_ptr + offs + head_dim_div, o2, mask=q_k_mask)
+    stride_0 = num_key_value_heads * head_dim
+    offs_num_key_value_heads = tl.arange(0, num_key_value_heads)[None, :, None]
+    offs = offs_token * stride_0 + offs_num_key_value_heads * head_dim + offs_dim
+
+    k1 = tl.load(key_ptr + offs, mask=q_k_mask)
+    k2 = tl.load(key_ptr + offs + head_dim_div, mask=q_k_mask)
+    o1 = k1 * cos - k2 * sin
+    o2 = k2 * cos + k1 * sin
+    tl.store(key_ptr + offs, o1, mask=q_k_mask)
+    tl.store(key_ptr + offs + head_dim_div, o2, mask=q_k_mask)
+
+def rope_forward(query, key, sin, cos, max_context_length, offset):
+    batch_size, num_tokens, num_heads, head_dim = query.shape
+    batch_size, num_tokens, num_key_value_heads, head_dim = key.shape
+
+    grid = lambda META: (batch_size, triton.cdiv(num_tokens, META['TILE_TOKENS']))
+    rotate[grid](
+        query,
+        key,
+        sin,
+        cos,
+        offset,
+        max_context_length,
+        num_tokens,
+        num_heads,
+        head_dim,
+        num_key_value_heads,
+        head_dim // 2,
+        TILE_TOKENS = 32,
+    )
