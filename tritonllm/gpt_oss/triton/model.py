bfb158e3470e091606c645b2e09005314f0551a7
BobHuang <git@bobhuang.xyz>
2025-08-24 19:38:36 +0800

    torch.nn.Linear use class with torch.nn.Module

--- tritonllm/gpt_oss/triton/model.py
+++ tritonllm/gpt_oss/triton/model.py
@@ -61,12 +61,44 @@ class UnEmbedding(torch.nn.Module):
         self.weight = torch.nn.Parameter(
             torch.empty((vocab_size, hidden_size), device=device, dtype=torch.bfloat16)
         )
-        torch.nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
 
-
-    @record_function("unembedding_triton")
+    @record_function("unembedding_linear")
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return unembedding_forward(x, self.weight)
+        return torch.nn.functional.linear(x, self.weight, bias=None)
+
+
+class QKV(torch.nn.Module):
+    def __init__(
+        self, hidden_size: int, qkv_dim: int, device: torch.device | None = None
+    ):
+        super().__init__()
+        self.weight = torch.nn.Parameter(
+            torch.empty((qkv_dim, hidden_size), device=device, dtype=torch.bfloat16)
+        )
+        self.bias = torch.nn.Parameter(
+            torch.empty((qkv_dim), device=device, dtype=torch.bfloat16)
+        )
+
+    @record_function("qkv_linear")
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.nn.functional.linear(x, self.weight, self.bias)
+
+
+class OUT(torch.nn.Module):
+    def __init__(
+        self, out_dim: int, hidden_size: int, device: torch.device | None = None
+    ):
+        super().__init__()
+        self.weight = torch.nn.Parameter(
+            torch.empty((hidden_size, out_dim), device=device, dtype=torch.bfloat16)
+        )
+        self.bias = torch.nn.Parameter(
+            torch.empty((hidden_size), device=device, dtype=torch.bfloat16)
+        )
+
+    @record_function("out_linear")
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return torch.nn.functional.linear(x, self.weight, self.bias)
 
 
 class RotaryEmbedding(torch.nn.Module):
@@ -224,15 +256,8 @@ class AttentionBlock(torch.nn.Module):
         qkv_dim = config.head_dim * (
             config.num_attention_heads + 2 * config.num_key_value_heads
         )
-        self.qkv = torch.nn.Linear(
-            config.hidden_size, qkv_dim, device=device, dtype=torch.bfloat16
-        )
-        self.out = torch.nn.Linear(
-            config.head_dim * config.num_attention_heads,
-            config.hidden_size,
-            device=device,
-            dtype=torch.bfloat16,
-        )
+        self.qkv = QKV(config.hidden_size, qkv_dim, device=device)
+        self.out = OUT(config.head_dim * config.num_attention_heads, config.hidden_size, device=device)
         self.sm_scale = 1 / math.sqrt(config.head_dim)
         self.rope = RotaryEmbedding(
             config.head_dim,
@@ -569,7 +594,7 @@ class TokenGenerator:
         while max_tokens == 0 or num_generated_tokens < max_tokens:
             self.input_token[0] = predicted_token
             self.graph.replay()
-            torch.cuda.synchronize()
+            # torch.cuda.synchronize()
             predicted_token = self.sample_next_token(self.logits, temperature)
             num_generated_tokens += 1
 
